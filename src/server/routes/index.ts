7313879afd6b07288a26ba068d8ba103417de626
Eduardo-Pyment <edupymota@protonmail.com>
2024-01-29 23:14:45 -0300

    feat: http-status-code added to dependencies

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
     return res.send("Hello World!");
 });
 
-router.post("/", (req, res) => {
-    return res.send("Hello World!");
+router.post("/test", (req, res) => {
+    console.log(req.body);
+    return res.status(StatusCodes.ACCEPTED).json(req.body);
 });
 
 export { router };
+  
