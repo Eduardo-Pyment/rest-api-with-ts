6ad8a278c80cc8720c66c65a20c7ab7a8d82bcbc
Eduardo-Pyment <edupymota@protonmail.com>
2024-01-31 14:30:47 -0300

    feat: @ts ban-types switched off

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
 
-export const create = (req: Request, res: Response) => {
+interface ICities {
+  name: string;
+}
+
+export const create = (req: Request<{}, {}, ICities>, res: Response) => {
+
+  if (req.body.name === undefined || req.body.name === null || req.body.name === "") {
+    return res.status(400).send("Name is required!");
+  }
+
+  console.log(req.body);
 
   return res.send("Create!");
 };
