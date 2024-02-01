ec86380d2a11c89ff1ccff7ce36f20363496a896
Eduardo-Pyment <edupymota@protonmail.com>
2024-02-01 14:53:14 -0300

    feat: multiple error validation

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
 interface ICities {
@@ -15,15 +16,17 @@ export const create = async (req: Request<{}, {}, ICities>, res: Response) => {
   let validatedData: ICities | undefined = undefined;
   try {
     validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
-    });
-  }
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const validationErrors: Record<string, string> = {};//
 
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      validationErrors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors: validationErrors });
+  }
 
   console.log(validatedData);
 
