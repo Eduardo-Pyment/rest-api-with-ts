e7e74943a606b59fbd012909928063cffae8910e
Eduardo-Pyment <edupymota@protonmail.com>
2024-02-10 20:34:34 -0300

    feat: created first test case and fixed typos

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -19,5 +19,5 @@ export const createValidation = validation((getSchema) => ({
 export const create: RequestHandler = async (req: Request<{}, {}, ICities>, res: Response) => {
   console.log(req.body);
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Not implemented");
+  return res.status(StatusCodes.CREATED).json(1);
 };
