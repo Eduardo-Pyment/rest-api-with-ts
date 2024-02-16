4a29ba1bac00b3e1b65e17596f60bbfffb926e4a
Eduardo-Pyment <edupymota@protonmail.com>
2024-02-16 16:37:04 -0300

    feat: Creates models index an Cities. models are to be used as types by knex migrations.

--- src/server/database/knex/@types/types.d.ts
+++ src/server/database/knex/@types/types.d.ts
@@ -1,6 +1,7 @@
+import { ICities } from "../../models";
 declare module "knex/types/tables" {
   interface Tables{
-  //  cities: ICities
+   cities: ICities
   //  person: ICities 
   //  user: ICities 
   }
---
+++ src/server/database/models/Cities.ts
@@ -0,0 +1,4 @@
+export interface ICities {
+  id: number;
+  nome: string;
+}
